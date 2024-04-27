48fc6ec8c6e75038138aac8d80d2d374c0149b21
MahdiAsadolahzade <mahdiasadi140@gmail.com>
2024-04-27 10:19:29 +0330

    Configuration

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportHeight: 1000,
+  viewportWidth:1920,
+  video: false,
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    excludeSpecPattern: ['**/1-getting-started' , '**/2-advanced-examples'],
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    
+  },
+})
